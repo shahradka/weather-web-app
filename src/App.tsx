acad0261d4760a558ca9ad3f21359f2302844d9b
Shahrad ka <shahrad.kashany@gmail.com>
2024-08-01 21:46:59 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "./css/styles.scss";
+
+const App = () => {
+    return <div>
+        Contact App  Assessment!
+    </div>
+}
+
+export default App;
---
+++ webpack.common.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: './src/index.tsx',
+    module:{
+        rules:[
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: 'ts-loader',
+                
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                  ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+                type: 'asset/images',
+              },
+              {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/fonts',
+              },
+        ]
+    },
+    resolve:{
+        extensions: [".tsx", ".ts", ".js"]
+    },
+    output:{
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './src/index.html'
+        })
+      ]
+}
