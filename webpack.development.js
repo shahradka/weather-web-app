428db438e60bf9669d151a4eb01e63926825394a
shahrad kashani <shahrad.kashani@wallit.eu>
2024-08-16 17:35:24 +0330

    Bug(webpack dev): fix dev server port

--- webpack.development.js
+++ webpack.development.js
@@ -5,10 +5,8 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
     mode: "development",
     devtool: "inline-source-map",
-    devServer: {
-        port: 8083,
-      },
     devServer:{
+        port: 8083,
         client:{
             logging: "info",
             overlay: true
